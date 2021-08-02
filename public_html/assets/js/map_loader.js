0c5183c8e6a733c366aa64418a2804c9f084d233
Sherwin Sandoval <33995402+eSmooothie@users.noreply.github.com>
2021-08-02 09:53:41 +0800

    Change map style

--- public_html/assets/js/map_loader.js
+++ public_html/assets/js/map_loader.js
@@ -102,10 +102,8 @@ $(document).ready(async function(){
 
   var mymap = L.map('map').setView(LatLng, zoom);
 
-  var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-  	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-  	subdomains: 'abcd',
-  	maxZoom: 19
+  var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
   });
 
   sites.forEach((item, i) => {
@@ -114,5 +112,5 @@ $(document).ready(async function(){
   });
 
 
-  CartoDB_Voyager.addTo(mymap);
+  Esri_WorldImagery.addTo(mymap);
 });
